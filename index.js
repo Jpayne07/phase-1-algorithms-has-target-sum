4adaf1523430c4437327b199747e8790428268ca
Jacob <jacobpayne007@gmail.com>
2024-05-14 22:08:37 -0500

    not too bad

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(i=0;i< array.length;i++){
+    let first = array[i]
+    for (let j =i+1;j<array.length;j++){
+      let second = array[j]
+      console.log(second)
+      if(first+second === target){
+        return true
+      }
+    }
+  }
+  return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +19,25 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  for(i=0,i< array.length,i++){
+    let first = array[i]
+    for i=1,i<array.length,i++{
+      let second = array[i]
+      if(first+second !==target){
+        break
+
+      }
+      else{
+        return true
+      }
+    }
+  }
 */
 
 /*
   Add written explanation of your solution here
+  needs to define array length
+  will iterate through the first element of the array and summing the sequential one, if it matches then it will return true, 
 */
 
 // You can run `node index.js` to view these console logs
